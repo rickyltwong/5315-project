ae0cf314e88ad12066f399fe773151b5bd521f14
Ricky Wong <rickywonglt15@outlook.com>
2024-04-07 18:59:05 -0400

    Added declaration

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,3 +1,13 @@
+/******************************************************************************
+ ITE5315 – Project
+ I declare that this assignment is my own work in accordance with Humber Academic
+ Policy.
+ No part of this assignment has been copied manually or electronically from any other
+ source
+ (including web sites) or distributed to other students.
+ Name: Ricky L. T. Wong Student ID: N01581738 Date: 2024-04-07
+ ******************************************************************************/
+
 const { isObjectIdOrHexString } = require('mongoose');
 
 module.exports = {
