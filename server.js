6c15f71abf333cacfa1c18f72b6a6c74821fdaae
Ricky Wong <rickywonglt15@outlook.com>
2024-04-11 22:22:56 -0400

    Debug server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-process.on('uncaughtException', (error) => {
-  console.error('Uncaught Exception:', error);
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...', err);
   process.exit(1);
 });
 
@@ -23,23 +23,22 @@ const app = require('./app');
 const restaurantDb = require('./config/database');
 const PORT = process.env.PORT || 3000;
 
-const main = async () => {
-  await restaurantDb.initialize(process.env.DB_CONNECTION_STRING || "");
-  console.log('Database initialized successfully');
+let server;
 
-  return app.listen(PORT, '0.0.0.0',() => {
-    console.log(`Server running on port ${PORT}`);
+restaurantDb.initialize(process.env.DB_CONNECTION_STRING || "")
+  .then(() => {
+    console.log('Database initialized successfully');
+    server = app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to initialize database or start the server:', error);
+    process.exit(1);
   });
-};
 
-const server = main().catch((error) => {
-  console.error('Failed to start the server:', error);
-  process.exit(1);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.log('Unhandled rejection! Shutting down...');
-  console.log(err.name, err.message);
+process.on('unhandledRejection', err => {
+  console.log('Unhandled rejection! Shutting down...', err);
   server.close(() => {
     process.exit(1);
   });
